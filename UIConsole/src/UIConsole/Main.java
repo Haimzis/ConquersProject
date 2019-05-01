3d4f98f0d91f3f3d83a2929431555b2be02ed0cc
Haim Zisman <haimzismann@gmail.com>
2019-05-01 23:15:16 +0300

    check4

--- UIConsole/src/UIConsole/Main.java
+++ UIConsole/src/UIConsole/Main.java
@@ -3,7 +3,6 @@ package UIConsole;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Haim");
         UIConsole console = new UIConsole();
         console.showMainMenu();
     }
