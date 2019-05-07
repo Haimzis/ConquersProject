53c6ed29a491bf1c04aa8e108d64c08aa5b2ce16
Ran Tzur <rantzrur@gmail.com>
2019-05-07 17:48:33 +0300

    Changes to GD - for haim. Working on buying units popup.

--- UIJavaFX/src/SubComponents/Popups/BuyUnitsPopup/BuyUnitsPopupController.java
+++ UIJavaFX/src/SubComponents/Popups/BuyUnitsPopup/BuyUnitsPopupController.java
@@ -1,5 +1,16 @@
 package SubComponents.Popups.BuyUnitsPopup;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.SplitMenuButton;
+
 public class BuyUnitsPopupController {
+    private SplitMenuButton unitChoices;
+
+    @FXML
+    private void OnUnitsDropdownPressEvent(ActionEvent event) {
+        
+    }
 
 }
