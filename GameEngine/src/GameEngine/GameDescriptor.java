260a26e9e492888bb5e92fd13c6b212b479dfa86
Haim Zisman <haimzismann@gmail.com>
2019-05-07 17:49:28 +0300

    starting write

--- GameEngine/src/GameEngine/GameDescriptor.java
+++ GameEngine/src/GameEngine/GameDescriptor.java
@@ -85,16 +85,23 @@ public class GameDescriptor implements Serializable {
 
     private List<Player> loadPlayers(Generated.GameDescriptor descriptor) {
         List<Player> playersList = new ArrayList<>();
-        List<Generated.Player> players = descriptor.getPlayers().getPlayer();
-        for(Generated.Player player : players) {
-            int id;
-            String name;
+        if(descriptor.getPlayers() == null) {
+                Player playerOne = new Player(1 , "Ran", initialFunds);
+                Player playerTwo = new Player(2 , "Haim", initialFunds);
+                playersList.add(playerOne);
+                playersList.add(playerTwo);
+        } else {
+            List<Generated.Player> players = descriptor.getPlayers().getPlayer();
+            for(Generated.Player player : players) {
+                int id;
+                String name;
 
-            id = player.getId().intValue();
-            name = player.getName();
+                id = player.getId().intValue();
+                name = player.getName();
 
-            Player newPlayer = new Player(id , name, initialFunds);
-            playersList.add(newPlayer);
+                Player newPlayer = new Player(id , name, initialFunds);
+                playersList.add(newPlayer);
+            }
         }
         return playersList;
     }
@@ -207,6 +214,8 @@ public class GameDescriptor implements Serializable {
 
     private boolean validatePlayers(Generated.GameDescriptor descriptor) {
         Set<Integer> playerIdsSet = new HashSet<>();
+        if(descriptor.getPlayers() == null)
+            return true;
         if(descriptor.getPlayers().getPlayer().size() < 2 || descriptor.getPlayers().getPlayer().size() > 4)
             return false;
         for(int i = 0; i < descriptor.getPlayers().getPlayer().size(); i++) {
