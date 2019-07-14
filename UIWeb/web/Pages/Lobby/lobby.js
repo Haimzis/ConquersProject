02a0d3b63ce565fd1576327e1f2d6bf6e94e987f
Ran <rantzrur@gmail.com>
2019-07-14 17:28:32 +0300

    Starting

--- UIWeb/web/Pages/Lobby/lobby.js
+++ UIWeb/web/Pages/Lobby/lobby.js
@@ -2,12 +2,17 @@ var chatVersion = 0;
 var refreshRate = 2000; //milli seconds
 var USER_LIST_URL = buildUrlWithContextPath("userslist");
 var CHAT_LIST_URL = buildUrlWithContextPath("chat");
+var USER_LOGOUT_URL = buildUrlWithContextPath("logoutId")
 
 //add a method to the button in order to make that form use AJAX
 //and not actually submit the form
 $(function() { // onload...do
-    //add a function to the submit event
-        $("#userstring").val("");
-        // by default - we'll always return false so it doesn't redirect the user.
-        return false;
+
 });
+
+function logOutClicked() {
+        $.get(USER_LOGOUT_URL , function (data) {
+
+        })
+}
+
