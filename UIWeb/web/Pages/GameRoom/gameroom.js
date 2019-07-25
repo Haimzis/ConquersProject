c15a2e9e828d17776115c3fbf4c0bea196424c61
Ran Tzur <rantzrur@gmail.com>
2019-07-25 08:36:17 +0300

    Fuck you haim #13

--- UIWeb/web/Pages/GameRoom/gameroom.js
+++ UIWeb/web/Pages/GameRoom/gameroom.js
@@ -46,16 +46,9 @@ function setChat() {
             timeout: 2000,
             error: function() {
                 console.error("Failed to submit");
-            },
-            success: function(r) {
-                //do not add the user string to the chat area
-                //since it's going to be retrieved from the server
-                //$("#result h1").text(r);
             }
         });
-
         $("#userstring").val("");
-        // by default - we'll always return false so it doesn't redirect the user.
         return false;
     });
 }
